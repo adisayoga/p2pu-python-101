ad454c5abfe904890bdb629377292feabdb426da
Adi Sayoga <adisayoga@gmail.com>
2013-01-25 16:20:46 +0800

    add exercise 2.2

---
+++ 02. Expressions/exercise-2.2.py
@@ -0,0 +1,10 @@
+# Exercise 2.2
+#
+# Write a program that uses raw_input to prompt a user for their name
+# and then welcomes them.
+#
+# Enter your name: Chuck
+# Hello Chuck
+
+name = raw_input('Enter your name: ')
+print 'Hello ' + name
